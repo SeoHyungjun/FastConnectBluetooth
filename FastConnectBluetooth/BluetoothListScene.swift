827fdf572a220dc1eddfff6d95c0b19124575ee5
SeoHyungJun <wewwe96@naver.com>
2020-05-31 20:24:49 +0900

    Project Setup

--- FastConnectBluetooth/BluetoothListScene.swift
+++ FastConnectBluetooth/BluetoothListScene.swift
@@ -8,14 +8,14 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct BluetoothListScene: View {
     var body: some View {
         Text("Hello, World!")
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
+struct BluetoothListScene_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        BluetoothListScene()
     }
 }

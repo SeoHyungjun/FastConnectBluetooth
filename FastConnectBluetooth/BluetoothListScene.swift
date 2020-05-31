154f7cc1754fb18f4ee16410b2033b61ceca72da
SeoHyungJun <wewwe96@naver.com>
2020-05-31 21:12:29 +0900

    bluetooth device list view add

--- FastConnectBluetooth/BluetoothListScene.swift
+++ FastConnectBluetooth/BluetoothListScene.swift
@@ -9,13 +9,30 @@
 import SwiftUI
 
 struct BluetoothListScene: View {
+    //custom 공유 데이터로 등록된 bluetoothstore 인스턴스
+    //뷰가 생성되는 시점에 공유 데이터 목록을 확인, 스토어 속성과 동일한 형식을 가진 객체가 등록되어있다면
+    //store에 자동으로 저장
+    @EnvironmentObject var store: BluetoothStore
+    
     var body: some View {
-        Text("Hello, World!")
+        NavigationView {
+            List(store.list) {
+                bluetooth in
+                //배열에 저장되어있는 bluetooth가 파라미터로 전달
+                //텍스트로 bluetooth 내용 전달
+                Text(bluetooth.content)
+            }
+            //modifier(swiftUI에서 특별한 메소드)를 사용해서
+            //속성 또는 뷰를 변경
+            .navigationBarTitle("My Bluetooth Device")
+        }
     }
 }
 
 struct BluetoothListScene_Previews: PreviewProvider {
     static var previews: some View {
         BluetoothListScene()
+        //프리뷰에서 사용할 bluetooth store를 공유 데이터로 등록
+            .environmentObject(BluetoothStore())
     }
 }

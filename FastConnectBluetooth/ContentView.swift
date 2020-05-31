4ae96ac14e32209a6165def386a727c7595412f0
SeoHyungJun <wewwe96@naver.com>
2020-05-31 20:22:21 +0900

    Initial Commit

---
+++ FastConnectBluetooth/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FastConnectBluetooth
+//
+//  Created by hyungjun on 2020/05/31.
+//  Copyright © 2020 hyungjun. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}

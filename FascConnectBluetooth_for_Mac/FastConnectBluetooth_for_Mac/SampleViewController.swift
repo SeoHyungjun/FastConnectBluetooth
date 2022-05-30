d6d8d78c153b293e935235a1443c5b884c1c961a
SeoHyungjun <wewwe96@naver.com>
2022-05-30 18:50:12 +0900

    FEAT : Initial Bluetooth setup using CoreBluetooth

--- FascConnectBluetooth_for_Mac/FastConnectBluetooth_for_Mac/SampleViewController.swift
+++ FascConnectBluetooth_for_Mac/FastConnectBluetooth_for_Mac/SampleViewController.swift
@@ -6,14 +6,17 @@
 //
 
 import Cocoa
+import CoreBluetooth
 
 class SampleViewController: NSViewController {
+    private var centralManager: CBCentralManager!
     
     @IBOutlet var resultTextView: NSTextView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         resultTextView.layer?.cornerRadius = 30
+        centralManager = CBCentralManager(delegate: self, queue: nil)
     }
     
     
@@ -23,7 +26,7 @@ class SampleViewController: NSViewController {
     
 }
 
-extension SampleViewController {
+extension SampleViewController: CBCentralManagerDelegate {
     
     static func freshController() -> SampleViewController {
         
@@ -35,5 +38,24 @@ extension SampleViewController {
             fatalError("Why cant i find SampleViewController? - Check Main.stroyboard")
         }
         return viewcontroller
+    }
+    
+    func centralManagerDidUpdateState(_ central: CBCentralManager) {
+        switch central.state {
+            case .unknown:
+                print("central.state is unknown")
+            case .resetting:
+                print("central.state is resetting")
+            case .unsupported:
+                print("central.state is unsupported")
+            case .unauthorized:
+                print("central.state is unauthorized")
+            case .poweredOff:
+                print("central.state is poweredOff")
+            case .poweredOn:
+                print("central.state is poweredOn")
+            @unknown default:
+                print("central.state default case")
+        }
     }
 }
